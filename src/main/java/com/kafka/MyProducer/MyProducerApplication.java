a4ab369846ebd678db06cc9ebc58d3e887f2db28
mds9903 <mohammed.sujan@nextuple.com>
2023-01-15 15:59:31 +0530

    messages getting produced to topic

--- src/main/java/com/kafka/MyProducer/MyProducerApplication.java
+++ src/main/java/com/kafka/MyProducer/MyProducerApplication.java
@@ -1,7 +1,10 @@
 package com.kafka.MyProducer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class MyProducerApplication {
@@ -10,4 +13,16 @@ public class MyProducerApplication {
 		SpringApplication.run(MyProducerApplication.class, args);
 	}
 
+	// to send messages using kafka template
+	@Bean
+	CommandLineRunner myCommandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+//            kafkaTemplate.send("myTopic", "hello from myCmdLineRunner");
+//            kafkaTemplate.send("myTopic", "hello again");
+
+			for (int i = 0; i < 10; i++) {
+				kafkaTemplate.send("type_2_myTopic", "for loop msg: #" + i);
+			}
+		};
+	}
 }
