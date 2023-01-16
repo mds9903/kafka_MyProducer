1653a5811ddbe6560b0ce7ef55293dcf3acd5d3f
mds9903 <mohammed.sujan@nextuple.com>
2023-01-16 09:30:39 +0530

    code cleanup

--- src/main/java/com/kafka/MyProducer/MyProducerApplication.java
+++ src/main/java/com/kafka/MyProducer/MyProducerApplication.java
@@ -17,12 +17,11 @@ public class MyProducerApplication {
 	@Bean
 	CommandLineRunner myCommandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
 		return args -> {
-//            kafkaTemplate.send("myTopic", "hello from myCmdLineRunner");
-//            kafkaTemplate.send("myTopic", "hello again");
-
-			for (int i = 0; i < 10; i++) {
-				kafkaTemplate.send("type_2_myTopic", "for loop msg: #" + i);
+			int i = 0;
+			while(i < 1000_000_000) {
+				kafkaTemplate.send("type_2_myTopic", "for loop msg: #" + i++);
 			}
 		};
 	}
+
 }
